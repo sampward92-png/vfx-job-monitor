4152b0ae9c778eaf768da05f24aa90c2027b61a2
sampward92-png <sampward92@gmail.com>
2026-03-08 20:31:57 +0000

    Add new job keywords and URLs to main.py

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 KEYWORDS = [
-"production assistant",
-"production coordinator",
-"graduate producer",
-"studio runner",
-"studio assistant",
-"production trainee"
+    "production assistant",
+    "production coordinator",
+    "junior production coordinator",
+    "graduate producer",
+    "junior producer",
+    "assistant producer",
+    "studio runner",
+    "runner",
+    "studio assistant",
+    "production trainee",
+    "production intern"
 ]
 
 URLS = [
-"https://boards-api.greenhouse.io/v1/boards/framestore/jobs",
-"https://api.lever.co/v0/postings/nexusstudios"
+    "https://boards-api.greenhouse.io/v1/boards/framestore/jobs",
+    "https://api.lever.co/v0/postings/nexusstudios",
+    "https://boards-api.greenhouse.io/v1/boards/insomniac/jobs",
+    "https://boards-api.greenhouse.io/v1/boards/jellyfishpictures/jobs",
+    "https://boards-api.greenhouse.io/v1/boards/ustwo/jobs",
+    "https://api.lever.co/v0/postings/sidefx",
+    "https://api.lever.co/v0/postings/atomiccartoons",
 ]
 
 def send_telegram(msg):
